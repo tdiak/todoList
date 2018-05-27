600af937138f771dbfed91a7c275ef7c134d7023
Tomek <tomek.diakow@gmail.com>
2018-05-27 14:11:10 +0200

    Update admin

--- todolist/tasks/admin.py
+++ todolist/tasks/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Category, Task
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'color'
+    )
+    search_fields = (
+        'name',
+    )
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    pass
